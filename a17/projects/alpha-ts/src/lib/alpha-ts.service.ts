a0d90a10155372fd0af78925ec31c060e3462fe4
pierre <pierre@pvway.com>
2024-03-11 12:11:40 +0100

    alphaTs got AlphaLbs dependency

--- a17/projects/alpha-ts/src/lib/alpha-ts.service.ts
+++ a17/projects/alpha-ts/src/lib/alpha-ts.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {AlphaLbsService} from "@pvway/alpha-lbs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlphaTsService {
 
-  constructor() { }
+  constructor(lbs: AlphaLbsService) {
+    lbs.subscribe(
+      payload => console.log(payload));
+  }
 }
