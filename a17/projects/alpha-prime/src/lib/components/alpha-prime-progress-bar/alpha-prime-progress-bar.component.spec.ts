99183eec3ab17639645eaefebdcbb600ccb60f02
pierre <pierre@pvway.com>
2024-04-18 13:15:14 +0200

    Update AlphaPrimeProgressBarComponent for better display control
    This update adds a delay feature and show/hide functionality to the AlphaPrimeProgressBarComponent. The delay helps to prevent unnecessary flashing of the progress bar in case of very short operations. Additionally, unit tests have been significantly expanded to cover these new features. Version updated to 15.0.11 for alpha-prime.

--- a17/projects/alpha-prime/src/lib/components/alpha-prime-progress-bar/alpha-prime-progress-bar.component.spec.ts
+++ a17/projects/alpha-prime/src/lib/components/alpha-prime-progress-bar/alpha-prime-progress-bar.component.spec.ts
@@ -11,13 +11,57 @@ describe('AlphaPrimeProgressBarComponent', () => {
       imports: [AlphaPrimeProgressBarComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(AlphaPrimeProgressBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  })
+
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component["_busy"]).toBeFalsy();
+    expect(component.busy).toBeFalsy();
+    expect(component['tOut']).toBeUndefined();
+    expect(component.value).toEqual(100);
   });
+
+  describe('handle show progress bar', () => {
+
+    it('should handle show in determinate mode', () => {
+      component['tOut'] = {};
+      component.mode = 'determinate';
+      component.busy = true;
+      expect(component['_busy']).toBeTruthy();
+      expect(component['tOut']).toBeUndefined();
+    });
+
+    it('should handle show in non determinate mode', () => {
+      component['tOut'] = {};
+      component.mode = 'indeterminate';
+      component.busy = true;
+      expect(component['_busy']).toBeFalsy();
+      expect(component['tOut']).toBeDefined();
+      jest.advanceTimersByTime(2000);
+      expect(component.busy).toBeTruthy();
+      expect(component['tOut']).toBeUndefined();
+    });
+
+  });
+
+  it ('should handle hide', () => {
+    component['tOut'] = {};
+    component['_busy'] = true;
+    component.busy = false;
+    expect(component['_busy']).toBeFalsy();
+    expect(component['tOut']).toBeUndefined();
+  });
+
 });
--- a17/projects/alpha-prime/src/lib/components/alpha-prime-progress-bar/alpha-prime-progress-bar.component.ts
+++ a17/projects/alpha-prime/src/lib/components/alpha-prime-progress-bar/alpha-prime-progress-bar.component.ts
@@ -14,8 +14,48 @@ import {NgIf} from "@angular/common";
 })
 export class AlphaPrimeProgressBarComponent {
 
-  @Input() busy = false;
+  private tOut: any | undefined;
+  private _busy = false;
+
+  @Input()
+  set busy(busy: boolean) {
+    if (this.tOut){
+      clearTimeout(this.tOut);
+      this.tOut = undefined;
+    }
+    if (busy) {
+      this.showProgressBar();
+    } else {
+      this.hideProgressBar();
+    }
+  }
+  get busy(): boolean {
+    return this._busy;
+  }
+
+  @Input() delay = 2000;
   @Input() value = 100;
   @Input() mode: 'determinate' | 'indeterminate' | 'tiny' = 'indeterminate';
+
+  private showProgressBar(): void {
+    // start showing (directly or after a short delay)
+    if (this.mode === 'determinate') {
+      // we are in determinate mode.
+      // there should be no delay
+      this._busy = true;
+    } else {
+      // in other modes there should be a small
+      // delay before activating the bar
+      this.tOut = setTimeout(
+        () => {
+          this._busy = true;
+          this.tOut = undefined;
+        }, this.delay);
+    }
+  }
+
+  private hideProgressBar(): void {
+    this._busy = false;
+  }
 
 }
