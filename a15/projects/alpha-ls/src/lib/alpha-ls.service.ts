ccec250f07051949167d5337fc9068f918b831f6
pierre <pierre@pvway.com>
2024-04-24 15:02:02 +0200

    Update AlphaLbsService to implement IAlphaLoggerService
    AlphaLbsService has been updated to implement the IAlphaLoggerService interface from the @pvway/alpha-common dependency. The package dependencies in package.json and package-lock.json have been updated accordingly to ensure this functionality and compatibility. These changes include detailed documentation for the added methods.

--- a15/projects/alpha-ls/src/lib/alpha-ls.service.ts
+++ a15/projects/alpha-ls/src/lib/alpha-ls.service.ts
@@ -1,6 +1,7 @@
 import {catchError, throwError} from "rxjs";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {IAlphaLoggerService} from "@pvway/alpha-common";
 
 class UsoLog {
   context: string;
@@ -34,23 +35,44 @@ class UsoNavigationLog {
 @Injectable({
   providedIn: 'root'
 })
-export class AlphaLsService {
+export class AlphaLsService
+  implements IAlphaLoggerService {
 
   constructor(
     private mHttp: HttpClient) {
   }
 
+  /**
+   * Initialize the service with optional
+   * error log and navigation log URLs.
+   *
+   * @param {string} [postErrorLogUrl] - The URL to which error logs will be posted.
+   * @param {string} [postNavigationLogUrl] - The URL to which navigation logs will be posted.
+   *
+   * @return {void}
+   */
   init(postErrorLogUrl?: string,
-       postNavigationLogUrl?: string) {
+       postNavigationLogUrl?: string):void {
     this._postErrorLogUrl = postErrorLogUrl ?? null;
     this._postNavigationLogUrl = postNavigationLogUrl ?? null;
   }
 
   private _postNavigationLog = this.postNavigationLogBuiltIn;
   private _postNavigationLogUrl: string | null = null;
-  usePostNavigationLog(postNavigationLog: (path: string, title: string) => any) {
+  /** Inject your own delegate for
+   * posting navigation logs */
+  usePostNavigationLog(
+    postNavigationLog: (
+      path: string, title: string) => any):void {
     this._postNavigationLog = postNavigationLog;
   }
+  /**
+   * Posts the navigation log for a specific path and title.
+   *
+   * @param {string} path - The path of the navigation.
+   * @param {string} title - The title of the navigation.
+   * @return {void}
+   */
   postNavigationLog(path: string, title: string): void {
     this._postNavigationLog(path, title);
   }
@@ -72,12 +94,29 @@ export class AlphaLsService {
     });
   }
 
-  usePostErrorLog(postErrorLog: (context: string, method: string, error: string) => any) {
+  /** Inject your own delegate for posting errors */
+  usePostErrorLog(
+    postErrorLog: (
+      context: string,
+      method: string,
+      error: string) => any): void {
     this._postErrorLog = postErrorLog;
   }
   private _postErrorLog = this.postErrorLogBuiltIn;
   private _postErrorLogUrl: string | null = null;
-  postErrorLog(context: string, method: string, error: string): void {
+
+  /**
+   * Posts an error log with the specified context, method, and error message.
+   *
+   * @param {string} context - The context in which the error occurred.
+   * @param {string} method - The name of the method or function where the error occurred.
+   * @param {string} error - The error message or description.
+   * @return {void} - This method does not return a value.
+   */
+  postErrorLog(
+    context: string,
+    method: string,
+    error: string): void {
     this._postErrorLog(context, method, error);
   }
   private postErrorLogBuiltIn(
