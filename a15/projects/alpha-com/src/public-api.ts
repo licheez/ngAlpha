053781186bb2610cea96dd6bb9ea2bddf11defbe
pierre <pierre@pvway.com>
2024-05-03 07:59:58 +0200

    Remove unnecessary abstraction files and update package version
    Seven unnecessary abstraction files were removed within the alpha-com project library. These constituted extraneous code not needed for core functionalities. This cleaning up of the codebase improves maintainability, readability and optimizes memory. Alongside this, the package version was updated as part of the regular version control process.

--- a15/projects/alpha-com/src/public-api.ts
+++ a15/projects/alpha-com/src/public-api.ts
@@ -2,18 +2,11 @@
  * Public API Surface of alpha-com
  */
 
-export * from './lib/alpha-api-abstractions';
-export * from './lib/alpha-lbs-abstractions';
-export * from './lib/alpha-ls-abstractions';
-export * from './lib/alpha-ns-abstractions';
-export * from './lib/alpha-oas-abstractions';
-export * from './lib/alpha-ts-abstractions';
+export * from './lib/alpha-utils';
+export * from './lib/alpha-window';
+export * from './lib/alpha-ymd';
 
 export * from './lib/alpha-http-result';
 export * from './lib/alpha-http-result-notification';
 export * from './lib/alpha-mutation-enum';
 export * from './lib/alpha-severity-enum';
-
-export * from './lib/alpha-utils';
-export * from './lib/alpha-window';
-export * from './lib/alpha-ymd';
