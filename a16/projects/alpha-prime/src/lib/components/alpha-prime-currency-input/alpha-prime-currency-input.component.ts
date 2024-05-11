70f781091138a0bf3591b1f833de1d66e87b82fd
pierre <pierre@pvway.com>
2024-05-11 09:16:37 +0200

    Add multiple components to Alpha Prime library
    Added progress bar, number input, auto complete, cancel button, delete button, submit button, currency input, and password input components to the Alpha Prime library. This provides users with a variety of ready-to-use, pre-styled Angular components that can greatly simplify UI development process.

---
+++ a16/projects/alpha-prime/src/lib/components/alpha-prime-currency-input/alpha-prime-currency-input.component.ts
@@ -0,0 +1,134 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+import {NgClass, NgIf} from "@angular/common";
+
+import {InputTextModule} from "primeng/inputtext";
+import {KeyFilterModule} from "primeng/keyfilter";
+import {FormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
+import {AlphaPrimeService} from "../../services/alpha-prime.service";
+
+@Component({
+  selector: 'alpha-prime-currency-input',
+  standalone: true,
+  imports: [
+    NgIf,
+    InputTextModule,
+    KeyFilterModule,
+    NgClass,
+    FormsModule,
+    ButtonModule,
+    RippleModule
+  ],
+  templateUrl: './alpha-prime-currency-input.component.html',
+  styleUrls: ['./alpha-prime-currency-input.component.css']
+})
+export class AlphaPrimeCurrencyInputComponent {
+
+  @Input() name = this.mPs.generateRandomName();
+  @Input() disabled = false;
+  @Input() placeHolder = '';
+  @Input() min = - Number.MAX_VALUE;
+  @Input() max = Number.MAX_VALUE;
+  @Input() currency = 'EUR';
+  @Input() currencySymbol = '€';
+  @Input() locale = 'fr-BE';
+  @Input() readonly = false;
+  @Input() readonlyCaption: string | undefined = '';
+  @Input() sm = false;
+  @Input() showClear = true;
+
+  //https://www.regextester.com/
+  // noinspection RegExpRedundantEscape
+  realNumber: RegExp = /[0-9\,\.\-]/;
+
+  sVal: string | number | undefined;
+  @Input()
+  set value(value: number | undefined) {
+    switch (value) {
+      case undefined:
+        this.sVal = undefined;
+        break;
+      case 0:
+        this.sVal = '0';
+        break;
+      default:
+        this.sVal = value.toString();
+    }
+  }
+  get value(): number | undefined {
+    if (!this.sVal) {
+      return undefined;
+    }
+    const n = Number(this.sVal);
+    return isNaN(n) ? undefined : n;
+  }
+  @Output() valueChange = new EventEmitter<number>();
+
+  constructor(private mPs: AlphaPrimeService) {
+  }
+
+  invalid = false;
+
+  baseInputStyle = {
+    'width': '100%',
+    'border-top-right-radius': '0',
+    'border-bottom-right-radius': '0'
+  };
+
+  smInputStyle = {
+    'width': '100%',
+    'border-top-right-radius': '0',
+    'border-bottom-right-radius': '0',
+    'font-size': 'x-small'
+  };
+
+  get inputStyle(): any {
+    return this.sm
+      ? this.smInputStyle
+      : this.baseInputStyle;
+  }
+
+  onTextChange(sVal: string | number | undefined) {
+    if (!sVal) {
+      this.onClear();
+      return;
+    }
+    // make sure sVal is a string
+    sVal = `${sVal}`;
+    this.sVal = sVal.replaceAll(',', '.');
+    if (this.sVal === '-0'
+      || this.sVal === '-.'
+      || this.sVal === '-0.') {
+      this.invalid = false;
+      this.valueChange.emit(undefined);
+      return;
+    }
+    const n = Number(this.sVal);
+    let v: number | undefined;
+    if (isNaN(n)
+      || n < this.min
+      || n > this.max) {
+      this.invalid = true;
+      v = undefined;
+    } else {
+      this.invalid = false;
+      v = n;
+    }
+    this.valueChange.emit(v);
+  }
+
+  onClear(): void {
+    this.invalid = false;
+    this.sVal = undefined;
+    this.valueChange.emit(undefined);
+  }
+
+  onBlur(): void {
+    const v = this.value;
+    if (v) {
+      this.sVal = v.toFixed(2);
+    }
+  }
+}
---
+++ a16/projects/alpha-prime/src/lib/components/alpha-prime-date-picker/alpha-prime-date-picker.component.ts
@@ -0,0 +1,96 @@
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
+import {CalendarModule} from "primeng/calendar";
+import {FormsModule} from "@angular/forms";
+import {RippleModule} from "primeng/ripple";
+import {InputTextModule} from "primeng/inputtext";
+import {AlphaPrimeService} from "../../services/alpha-prime.service";
+
+@Component({
+  selector: 'alpha-prime-date-picker',
+  standalone: true,
+  imports: [
+    NgIf,
+    CalendarModule,
+    FormsModule,
+    RippleModule,
+    NgClass,
+    InputTextModule
+  ],
+  templateUrl: './alpha-prime-date-picker.component.html',
+  styleUrls: ['./alpha-prime-date-picker.component.css']
+})
+export class AlphaPrimeDatePickerComponent implements OnDestroy {
+
+  @Input() name = this.mPs.generateRandomName();
+
+  phInterval : any;
+  ph = 'dd/mm/yyyy';
+  @Input()
+  set placeHolder (ph: string | string[]) {
+    if (Array.isArray(ph)) {
+      let i = 0;
+      this.ph = ph[i];
+      this.phInterval = setInterval(
+        () => {
+          i++;
+          if (i >= ph.length) {
+            i = 0;
+          }
+          this.ph = ph[i];
+        }, 5000);
+    } else {
+      console.log('this is not an array');
+      this.ph = ph;
+    }
+  }
+
+  @Input() disabled = false;
+  @Input() showClear = true;
+  @Input() showTime = false;
+  @Input() showSeconds = false;
+  @Input() dateFormat = 'dd/mm/yy';
+  @Input() minDate: Date = new Date(1971,0,1);
+  @Input() maxDate: Date = new Date(3000,11,31);
+  @Input() date: Date | undefined;
+  @Input() readonly = false;
+  @Input() readonlyCaption: string | undefined | null = '';
+  @Input() sm = false;
+  @Output() dateChange = new EventEmitter<Date | undefined>();
+
+  constructor(private mPs: AlphaPrimeService) {}
+
+  baseInputStyle = {
+    'width': '100%',
+    'border-top-right-radius': '0',
+    'border-bottom-right-radius': '0'
+  };
+
+  smInputStyle = {
+    'width': '100%',
+    'border-top-right-radius': '0',
+    'border-bottom-right-radius': '0',
+    'font-size': 'x-small'
+  };
+
+  get inputStyle(): any {
+    return this.sm
+      ? this.smInputStyle
+      : this.baseInputStyle;
+  }
+
+  ngOnDestroy(): void {
+    if (this.phInterval) {
+      clearInterval(this.phInterval);
+    }
+  }
+
+  onClear(): void {
+    this.date = undefined;
+    this.dateChange.emit(undefined);
+  }
+
+  onDateChanged(dt: Date): void {
+    this.dateChange.emit(dt);
+  }
+}
