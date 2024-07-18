9b9f7b7e3e6131629b3015d5ae398d988b0dd7f4
pierre <pierre@pvway.com>
2024-07-18 13:25:39 +0200

    Add new AlphaLs Angular library
    A new Angular library named AlphaLs has been created along with all the necessary files for the setup including package.json, tsconfig.json, ng-package.json, and README.md. The library also contains a basic service and component, run and build instructions in the README, and test configurations.

---
+++ a18/projects/alpha-ls/src/lib/alpha-ls.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlphaLsComponent } from './alpha-ls.component';
+
+describe('AlphaLsComponent', () => {
+  let component: AlphaLsComponent;
+  let fixture: ComponentFixture<AlphaLsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AlphaLsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AlphaLsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
