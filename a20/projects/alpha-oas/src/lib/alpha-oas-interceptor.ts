093870a1848ea024e27daa1b4a1765bab7aed3d3
pierre <pierre@pvway.com>
2025-10-15 12:01:05 +0200

    Add AlphaOasInterceptor with session and local storage initialization

--- a20/projects/alpha-oas/src/lib/alpha-oas-interceptor.ts
+++ a20/projects/alpha-oas/src/lib/alpha-oas-interceptor.ts
@@ -5,31 +5,52 @@ import { AlphaSessionData } from "./alpha-session-data";
 
 export class AlphaOasInterceptor implements HttpInterceptor {
 
+  private mSessionStorage: Storage = sessionStorage;
+  private mLocalStorage: Storage = localStorage;
+
+  initStorage(
+    sStorage: Storage = sessionStorage,
+    lStorage: Storage = localStorage): void {
+    this.mSessionStorage = sStorage;
+    this.mLocalStorage = lStorage;
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
-    const eReq = AlphaOasInterceptor.enrichReq(req);
+    const eReq =
+      AlphaOasInterceptor.enrichReq(
+        req, this.mSessionStorage, this.mLocalStorage);
     return next.handle(eReq);
   }
 
-  static handlerFn(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
-    const eReq = this.enrichReq(req);
+  static handlerFn(
+    req: HttpRequest<any>,
+    next: HttpHandlerFn,
+    sStorage: Storage = sessionStorage,
+    lStorage: Storage = localStorage): Observable<HttpEvent<any>> {
+    const eReq = this.enrichReq(req, sStorage, lStorage);
     return next(eReq);
   }
 
-  private static enrichReq(req: HttpRequest<any>): HttpRequest<any> {
+  private static enrichReq(
+    req: HttpRequest<any>,
+    sStorage: Storage,
+    lStorage: Storage): HttpRequest<any> {
+
     let headers = req.headers;
 
     // getting languageCode
     // set by the principal when setting the user
-    let languageCode =
-      sessionStorage.getItem('alphaLanguageCode');
+    let languageCode = sStorage.getItem('alphaLanguageCode');
     if (!languageCode) {
       const nav = window.navigator as any;
       const userLang = (nav.language || nav.userLanguage) as string;
       languageCode = userLang
         ? userLang.substring(0, 2)?.toLowerCase()
         : 'en';
+      sStorage.setItem('alphaLanguageCode', languageCode);
     }
+
     // always add the language-code header
     headers = headers.append('language-code', languageCode);
 
@@ -41,11 +62,11 @@ export class AlphaOasInterceptor implements HttpInterceptor {
      * ClientId is only generated once and stored in
      * the browser localstorage associated to the url
      */
-    let clientId = localStorage.getItem('alphaClientId');
+    let clientId = lStorage.getItem('alphaClientId');
     if (clientId == null)
     {
       clientId = uuidV4();
-      localStorage.setItem('alphaClientId', clientId!);
+      lStorage.setItem('alphaClientId', clientId!);
     }
     // always add the client-id header
     headers = headers.append('client-id', clientId!);
@@ -54,7 +75,7 @@ export class AlphaOasInterceptor implements HttpInterceptor {
      * when an accessToken is present insert the authorization header
      * using the accessToken with the bearer scheme
      */
-    const sd = AlphaSessionData.retrieve();
+    const sd = AlphaSessionData.retrieve(sStorage);
     const token = sd?.accessToken;
     if (token) {
       headers = headers.append(
