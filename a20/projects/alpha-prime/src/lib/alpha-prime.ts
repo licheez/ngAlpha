2740344648f950d2fdccc925261f3195cb74d9d5
pierre <pierre@pvway.com>
2025-10-17 10:43:37 +0200

    Add AlphaPrime component with unit tests and initial configuration

---
+++ a20/projects/alpha-prime/src/lib/alpha-prime.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-AlphaPrime',
+  imports: [],
+  template: `
+    <p>
+      alpha-prime works!
+    </p>
+  `,
+  styles: ``
+})
+export class AlphaPrime {
+
+}
