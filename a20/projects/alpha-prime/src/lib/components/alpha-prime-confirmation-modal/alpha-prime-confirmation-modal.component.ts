89a48f41a7258f11490b3ccc974a33860655f3cf
pierre <pierre@pvway.com>
2025-12-17 16:07:22 +0100

    Add AlphaPrimeDebugTag component with visibility control and unit tests

---
+++ a20/projects/alpha-prime/src/lib/components/alpha-prime-confirmation-modal/alpha-prime-confirmation-modal.component.ts
@@ -0,0 +1,73 @@
+import {Component, OnInit} from '@angular/core';
+import {AlphaPrimeService} from '../../services/alpha-prime.service';
+import {DynamicDialogConfig, DynamicDialogRef} from 'primeng/dynamicdialog';
+import {ButtonDirective, ButtonLabel} from 'primeng/button';
+import {AlphaPrimeDebugTagComponent} from '../alpha-prime-debug-tag/alpha-prime-debug-tag.component';
+
+@Component({
+  selector: 'alpha-prime-confirmation-modal',
+  imports: [
+    ButtonDirective,
+    ButtonLabel,
+    AlphaPrimeDebugTagComponent
+  ],
+  templateUrl: './alpha-prime-confirmation-modal.component.html',
+  styleUrls: ['./alpha-prime-confirmation-modal.component.css']
+})
+export class AlphaPrimeConfirmationModalComponent implements OnInit {
+  onClose: (confirmed: boolean) => any = () => { };
+
+  message: string;
+  yes: string;
+  no: string;
+
+  constructor(
+    private mPs: AlphaPrimeService,
+    private mDdr: DynamicDialogRef,
+    private mDdc: DynamicDialogConfig) {
+    this.message = this.mPs.getTr('alpha.confirmationModal.message');
+    this.yes = this.mPs.getTr('alpha.confirmationModal.yes');
+    this.no = this.mPs.getTr('alpha.confirmationModal.no');
+  }
+
+  ngOnInit() {
+    this.mDdc.data.setInstance(this);
+  }
+
+  init(
+    onClose: (confirmed: boolean) => any,
+    title?: string,
+    message?: string,
+    yes?: string,
+    no?: string) {
+    this.onClose = onClose;
+
+    setTimeout(
+      () => {
+        this.mDdc.header = title
+          ? title
+          : this.mPs.getTr('alpha.confirmationModal.title');
+      }, 100);
+
+    if (message) {
+      this.message = message;
+    }
+    if (yes) {
+      this.yes = yes;
+    }
+    if (no) {
+      this.no = no;
+    }
+  }
+
+  onYes(): void {
+    this.onClose(true);
+    this.mDdr.close(true);
+  }
+
+  onNo(): void {
+    this.onClose(false);
+    this.mDdr.close(false);
+  }
+
+}
